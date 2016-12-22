4984e54c2c8336af2690e08646b5f5929d0c3461
maryna.yelakova <maryna.yelakova@gmail.com>
2016-12-22 20:06:20 +0200

    fix 2way-binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -14,74 +14,77 @@ export class Day {
                 <span class="badge">{{day.id}}</span>{{day.name}}
             </li>
         </ul>  
-        <div *ngIf="secetedDay">
+        <div *ngIf="selectedDay">
             <h2>{{selectedDay.name}}</h2>
             <div>
-                <label>day:</label>
+                <label>day: </label>
                 <input [(ngModel)]="selectedDay.name" placeholder="day"/>
             </div>
-        </div>    
+        </div>       
             `,
-    styles:[`
-        .selected {
-    background-color:#CFD8DC !important;
-    color:white;
-        }
-        .days {
-        margin:0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-        }
-        .days li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0;
-        height: 1.6em;
-        border-radius: 4px;
-        }
-        .days li.selected:hover{
-        background-color: #BBDBDC; !important;
-        color: white;
-        }
-        .days li:hover {
-        color: #607d8b;
-        background-color: #DDD;
-        left: .1em;
-        }
-        .days .text {
-        position: relative;
-        top: -3px;
-        }
-        .days .badge {
-        dysplay:inline-block;
-        font-size: small;
-        color:white;
-        padding: 0.8em 0.7em 0 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left:-1px;
-        top: 0px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0 0 4px;
-        }
-    `]
+    styles: [`
+     .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+    }
+     .days {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+    }
+     .days li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+    }
+    .days li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+     }
+    .days li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+    }
+    .days .text {
+    position: relative;
+    top: -3px;
+    }
+    .days .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export  class AppComponent {
     title = 'Home task';
     selectedDay: Day;
     public days = DAYS;
-    onSelect(day: Day) { this.selectedDay = day; }
+    onSelect(day: Day): void {
+        this.selectedDay = day;
+    }
 }
 
 var DAYS: Day[] = [
-    {"id":2, "name":"Tuesday"},
-    {"id":3, "name":"Wednesday"},
-    {"id":4, "name":"Thursday"},
-    {"id":5, "name":"Friday"}
+    { "id":1, "name":"Monday" },
+    { "id":2, "name":"Tuesday" },
+    { "id":3, "name":"Wednesday" },
+    { "id":4, "name":"Thursday" },
+    { "id":5, "name":"Friday" }
 ];
